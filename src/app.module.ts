bdc5c7fc5595e8ee13045ec214a37b803c533f7e
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-05-26 15:36:36 +0700

    MVC module and connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://thanhtc1:d3E4XvIwZsw4GRLK@cluster0.m8myntw.mongodb.net/'),
+    
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+    isGlobal: true
+    })
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
