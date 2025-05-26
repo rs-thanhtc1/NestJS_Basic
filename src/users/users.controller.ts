9bbc7665bc02df2b929d8c015fee22b55ac6cb91
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-05-26 18:01:16 +0700

    creat a new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(
+    @Body("email") email: string,  // tương ứng với const email = req.body.email
+    @Body("password") password: string,
+    @Body("name") name: string
+  ) {
+
+    return this.usersService.create(email, password, name);
   }
 
   @Get()
