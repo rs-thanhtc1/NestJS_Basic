34926d60be3a7d3dc36af395022d1542a7d50ee6
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-05-28 17:35:50 +0700

    use pipe validate data

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 // dto -> data transfer object
 export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsNotEmpty()
     password: string;
+    
     name: string;
     address: string;
 }
