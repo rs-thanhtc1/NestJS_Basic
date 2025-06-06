022870e99373695d596447202116f83183d75f00
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-06-06 10:02:06 +0700

    generate modules company

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty } from 'class-validator';
+
+// dto -> data transfer object
+export class CreateCompanyDto {
+    @IsNotEmpty({message: 'Name không được để trống'})
+    name: string;
+
+    @IsNotEmpty({message: 'Address không được để trống'})
+    address: string;
+    
+    @IsNotEmpty({message: 'Description không được để trống'})
+    description: string;
+}
