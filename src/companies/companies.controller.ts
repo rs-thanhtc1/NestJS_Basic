29e04bc6cb765735d94263d8e8f011343a4e4ca7
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-06-09 14:08:18 +0700

    customize message

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestj
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-import { User } from 'src/decorator/customize';
+import { ResponseMessage, User } from 'src/decorator/customize';
 
 @Controller('companies')
 export class CompaniesController {
@@ -14,6 +14,7 @@ export class CompaniesController {
   }
 
   @Get()
+  @ResponseMessage("Fetch List Company with paginate")
   findAll(
     @Query("page") currentPage: string, // -> const currentPage: string = req.query.page
     @Query("limit") limit: string,       // -> const limit: string = req.query.limit
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  const reflector = app.get( Reflector );
+  const reflector = app.get(Reflector);
   app.useStaticAssets(join(__dirname, '..', 'public')); // js, css, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // view
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalGuards( new JwtAuthGuard( reflector ) );
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   // config cors
   app.enableCors(
