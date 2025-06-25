dc4e1d3b717d82d339554052cdf8faa1bf8bbf4a
rs-thanhtc1 <thanhtc1@runsystem.net>
2025-06-25 10:36:29 +0700

    fetch subscriber by skills

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { PermissionsModule } from './permissions/permissions.module';
 import { RolesModule } from './roles/roles.module';
 import { DatabasesModule } from './databases/databases.module';
 import { SubscribersModule } from './subscribers/subscribers.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -54,7 +55,9 @@ import { SubscribersModule } from './subscribers/subscribers.module';
 
     DatabasesModule,
 
-    SubscribersModule
+    SubscribersModule,
+
+    MailModule
    ],
   controllers: [AppController],
   providers: [AppService,
